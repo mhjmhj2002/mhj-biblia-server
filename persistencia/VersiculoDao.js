0f0d617969327fda92910dbafbee709dd8ddec8a
mhjmhj2002 <mhjmhj2002@gmail.com>
2018-09-30 19:05:58 -0300

    add query

--- persistencia/VersiculoDao.js
+++ persistencia/VersiculoDao.js
@@ -3,8 +3,8 @@ function VersiculoDao(connection) {
 }
 
 VersiculoDao.prototype.buscaPorId = function (version, liv, cap, vers, callback) {    
-    var query = " select * from versiculos where ver_vrs_id = ?  and ver_liv_id = ? and ver_capitulo = ? ";//and ver_versiculo = ? ";
-    this._connection.query(query, [version, liv, cap], callback);
+    var query = " select * from versiculos where ver_vrs_id = ?  and ver_liv_id = ? and ver_capitulo = ? and ver_versiculo = ? ";
+    this._connection.query(query, [version, liv, cap, vers], callback);
 }
 
 module.exports = function(){
